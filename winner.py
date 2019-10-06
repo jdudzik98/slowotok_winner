1e61081c7020035f1ec1b75aabff2e12b9b66cb5
jandudzik <dudzikjanek@gmail.com>
2019-10-06 19:40:08 +0200

    Changed convert_data function into default script activity. Added numbers-inputting mechanism, and function returning connections to given letter.

--- winner.py
+++ winner.py
@@ -1,22 +1,71 @@
-def convert_data():
 
-    """ Function turning words database into dict of dicts of lists for better efficiency"""
 
-    data = {}
+data = {}
 
-    letters = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó',
-               'p', 'q', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
+letters = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p',
+           'q', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
 
-    for single_letter_0 in letters:
-        data[str(single_letter_0)] = {}
-        for single_letter_1 in letters:
-            data[str(single_letter_0)][single_letter_1] = []
 
-    with open('slowa.txt') as file:
-        words_list = file.read().splitlines()
+for single_letter_0 in letters:
+    data[str(single_letter_0)] = {}
+    for single_letter_1 in letters:
+        data[str(single_letter_0)][single_letter_1] = []
 
-    for word in words_list:
-        if len(word) >= 3:
-           data[word[0]][word[1]].append(word[2:])
-    return data
+with open('slowa.txt') as file:
+    words_list = file.read().splitlines()
 
+for word in words_list:
+    if len(word) >= 3:
+        data[word[0]][word[1]].append(word[2:])
+
+
+game_letters = [[], [], [], []]
+
+for letter in range(16):
+    new_letter = input('Write letter number ' + str(letter + 1))
+    if len(new_letter) == 1 and new_letter in letters:
+        if letter <= 3:
+            game_letters[0].append(new_letter)
+        elif letter <= 7:
+            game_letters[1].append(new_letter)
+        elif letter <= 11:
+            game_letters[2].append(new_letter)
+        else:
+            game_letters[3].append(new_letter)
+    else:
+        print("That's not a single letter!")
+
+
+def find_connections(row, column):
+    """ Return a list of points, that you can connect given point with"""
+
+    connections = []
+    if row != 0:
+        connections.append([row-1, column])
+        if column != 0:
+            connections.append([row-1, column-1])
+        if column != 3:
+            connections.append([row-1, column+1])
+    if row != 3:
+        connections.append([row+1, column])
+        if column != 0:
+            connections.append([row+1, column-1])
+        if column != 3:
+            connections.append([row+1, column+1])
+    if column != 0:
+        connections.append([row, column-1])
+    if column != 3:
+        connections.append([row, column+1])
+
+    return connections
+
+
+"""
+
+for type in game_letters[0]:
+    used_letters = []
+    used_letters.append(type)
+    next = game_letters[0].index(type)
+    if next<=2:
+        print(game_letters[0][next+1])
+"""
