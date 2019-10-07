071e86ca49ce16d5d7aabe1bdd7457a07a4f156e
jandudzik <dudzikjanek@gmail.com>
2019-10-07 23:01:34 +0200

    Script printing out possible words, that you can make from given numbers (at this point, script is allowed to use every letter more than once)

--- winner.py
+++ winner.py
@@ -60,12 +60,23 @@ def find_connections(row, column):
     return connections
 
 
-"""
+for row in range(4):
+    for column in range(4):
+        for letter_type in find_connections(row, column):
+            if len(data[game_letters[row][column]][game_letters[letter_type[0]][letter_type[1]]]) >= 1:
+                for example in data[game_letters[row][column]][game_letters[letter_type[0]][letter_type[1]]]:
+                    current_position = [letter_type[0], letter_type[1]]
+                    for letter in range(len(example)):
+                        for x, y in find_connections(current_position[0], current_position[1]):
+                            if game_letters[x][y] == example[letter]:
+                                if letter == (len(example)-1):
+                                    print(game_letters[row][column] + game_letters[letter_type[0]][letter_type[1]] +
+                                          example)
+                                current_position = [x, y]
+                                break
 
-for type in game_letters[0]:
-    used_letters = []
-    used_letters.append(type)
-    next = game_letters[0].index(type)
-    if next<=2:
-        print(game_letters[0][next+1])
-"""
+                        if not game_letters[current_position[0]][current_position[1]] == example[letter]:
+                            break
+
+
+
